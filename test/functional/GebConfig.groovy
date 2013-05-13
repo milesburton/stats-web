2b9e19ce344b0a3638277dfe3aa800c2bd75de2f
Miles Burton <miles@mnetcs.com>
2013-05-13 09:04:18 +0200

    Update GebConfig.groovy

--- test/functional/GebConfig.groovy
+++ test/functional/GebConfig.groovy
@@ -8,10 +8,6 @@ import org.openqa.selenium.htmlunit.HtmlUnitDriver
 import org.openqa.selenium.firefox.FirefoxDriver
 import org.openqa.selenium.chrome.ChromeDriver
 
-if(!baseUrl){
-    baseUrl = "http://localhost:8080/stats-web/"
-}
-
 // Use htmlunit as the default
 // See: http://code.google.com/p/selenium/wiki/HtmlUnitDriver
 driver = { 
