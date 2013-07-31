9f8c594ccd38eecc7ac8f57c1c380a3ef9db9388
Miles Burton <miles@mnetcs.com>
2013-07-31 07:50:58 -0400

    @Miles made upload.sh generic

--- deployment/scripts/upload.sh
+++ deployment/scripts/upload.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-scp build/libs/stats-web.war deploy@qp2.agileview.co.uk:~/stats-web.war
-ssh deploy@qp2.agileview.co.uk "sudo chown tomcat7:tomcat7 /home/deploy/stats-web.war"
-ssh deploy@qp2.agileview.co.uk "sudo rm -rf /var/lib/tomcat7/webapps/stats-web*"
-ssh deploy@qp2.agileview.co.uk "sudo mv /home/deploy/stats-web.war /var/lib/tomcat7/webapps"
+scp build/libs/$WAR_NAME.war deploy@qp2.agileview.co.uk:~
+ssh deploy@qp2.agileview.co.uk "chown tomcat7:tomcat7 /home/deploy/$WAR_NAME.war"
+ssh deploy@qp2.agileview.co.uk "rm -rf /var/lib/tomcat7/webapps/$WAR_NAME*"
+ssh deploy@qp2.agileview.co.uk "mv ~/$WAR_NAME.war /var/lib/tomcat7/webapps"
+ssh deploy@qp2.agileview.co.uk "sudo /etc/init.d/tomcat7 restart"
 
