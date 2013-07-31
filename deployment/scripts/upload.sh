9dcfe683b573ab497be76a1bced02a360d724511
Miles Burton <miles@mnetcs.com>
2013-07-31 08:10:31 -0400

    @Miles removed chown

--- deployment/scripts/upload.sh
+++ deployment/scripts/upload.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 scp build/libs/$WAR_NAME.war deploy@qp2.agileview.co.uk:~
-ssh deploy@qp2.agileview.co.uk "chown tomcat7:tomcat7 /home/deploy/$WAR_NAME.war"
 ssh deploy@qp2.agileview.co.uk "rm -rf /var/lib/tomcat7/webapps/$WAR_NAME*"
 ssh deploy@qp2.agileview.co.uk "mv ~/$WAR_NAME.war /var/lib/tomcat7/webapps"
 ssh deploy@qp2.agileview.co.uk "sudo /etc/init.d/tomcat7 restart"
