b72651c6584060a8b1940dfc832466fee5a407c2
Miles Burton <miles@mnetcs.com>
2013-08-01 10:40:42 +0100

    Update upload.sh

--- deployment/scripts/upload.sh
+++ deployment/scripts/upload.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 ssh deploy@qp2.agileview.co.uk "sudo /etc/init.d/tomcat7 stop"
-#ssh deploy@qp2.agileview.co.uk "rm -rf /var/lib/tomcat7/webapps/$WAR_NAME*"
+ssh deploy@qp2.agileview.co.uk "rm /var/lib/tomcat7/webapps/$WAR_NAME.war"
 ssh deploy@qp2.agileview.co.uk "mv ~/$WAR_NAME.war /var/lib/tomcat7/webapps"
 ssh deploy@qp2.agileview.co.uk "sudo /etc/init.d/tomcat7 start"
