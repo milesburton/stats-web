cb70eb0c2a59fb96c7c29b2d0b1ee9d3b48e95ab
Miles Burton <miles@mnetcs.com>
2013-08-18 14:25:31 +0100

    Update upload.sh
    Auto installing apache config.

--- deployment/scripts/upload.sh
+++ deployment/scripts/upload.sh
@@ -2,6 +2,11 @@
 
 scp build/libs/$WAR_NAME.war deploy@qp2.agileview.co.uk:~
 ssh deploy@qp2.agileview.co.uk "sudo /etc/init.d/tomcat7 stop"
+ssh deploy@qp2.agileview.co.uk "test -f /etc/apache2/sites-enabled/$SITE || sudo a2dissite $SITE"
+ssh deploy@qp2.agileview.co.uk "test -f /etc/apache2/sites-available/$SITE || sudo rm /etc/apache2/sites-available/$SITE"
 ssh deploy@qp2.agileview.co.uk "rm /var/lib/tomcat7/webapps/$WAR_NAME.war"
+ssh deploy@qp2.agileview.co.uk "rm -rf /var/lib/tomcat7/webapps/$WAR_NAME"
 ssh deploy@qp2.agileview.co.uk "mv ~/$WAR_NAME.war /var/lib/tomcat7/webapps"
 ssh deploy@qp2.agileview.co.uk "sudo /etc/init.d/tomcat7 start"
+ssh deploy@qp2.agileview.co.uk "test -f /etc/apache2/sites-available/$SITE || sudo a2ensite $SITE"
+ssh deploy@qp2.agileview.co.uk "sudo service apache2 restart"
